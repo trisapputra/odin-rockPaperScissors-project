4217e74add17ca5c3d584478af01d6111c3e42ca
tri <trin.saputra@gmail.com>
2022-01-31 20:39:57 +0700

    finish the algorithm and function

--- javascript/main.js
+++ javascript/main.js
@@ -3,26 +3,68 @@ function computerPlay () {
     randomGenerator = Math.floor(Math.random() * 3);
 
     if (randomGenerator === 0) { 
-        computerWeapon = "rock"        
+        computerWeapon = "rock" ;       
     } else if (randomGenerator === 1) {
-        computerWeapon ="paper"
+        computerWeapon ="paper";
     } else { 
-        computerWeapon = "scissors"
+        computerWeapon = "scissors";
     };
 
     return computerWeapon;
 }
 
-let computerSelection = computerPlay();
+
 
 // Player Choose and assign into variable
-let playerWeapon = prompt("This is rock paper scisors game, choose your weapon !!");
-let playerSelection = playerWeapon.toLowerCase();
-
+function playerWeapon () {
+    return prompt("This is rock paper scisors game, choose your weapon !!")};
 
 // compare computer selection and player selection
-function game() {
-    if ( "rock" === "rock" ) {
-        alert("tie")
-    } else if ( "rock")
+
+let playerScore = 0;
+let computerScore = 0;
+function playRound () {
+
+
+    let computerSelection = computerPlay();
+
+
+    let playerSelection = playerWeapon().toLowerCase();
+    
+
+    if ( playerSelection === "rock" && computerSelection === "scissors" ) {
+        playerScore++;
+    } else if ( playerSelection === "rock" && computerSelection === "paper" ) {
+        computerScore++;
+    } else if ( playerSelection === "scissors" && computerSelection === "rock" ) {
+        computerScore++;
+    } else if ( playerSelection === "scissors" && computerSelection === "paper" ) {
+        playerScore++;
+    } else if ( playerSelection === "paper" && computerSelection === "rock" ) {
+        playerScore++;
+    }else if ( playerSelection === "paper" && computerSelection === "scissors" ){
+        computerScore++;
+    };
+
+    let  scoreBoard = playerScore + " " + ":" + " " + computerScore;
+
+    return alert(scoreBoard); 
 }
+
+
+playRound ();
+playRound ();
+playRound ();
+playRound ();
+playRound ();
+
+
+function matchScore () {
+
+    if (playerScore > computerScore) {
+        alert("player win");
+    } else if ( playerScore < computerScore) {
+        alert("you lose");
+    }
+};
+
