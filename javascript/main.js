7ed9237dc5f003f4b490b05980d7dad69c292376
tri <trin.saputra@gmail.com>
2022-04-07 05:21:53 +0700

    add eventlistener to button and rework playround function

--- javascript/main.js
+++ javascript/main.js
@@ -15,54 +15,39 @@ function computerPlay () {
 
 
 
-// Player Choose and assign into variable
-function playerWeapon () {
-    return prompt("This is rock paper scisors game, choose your weapon !!")};
-
-
-
-
-    
-
 // compare computer selection and player selection
 
 let playerScore = 0;
 let computerScore = 0;
-function playRound () {
+function playRound (e) {
 
 
     let computerSelection = computerPlay();
 
 
-    let playerSelection = playerWeapon().toLowerCase();
-    
+    let playerSelection = e.target.className;
+    console.log(playerSelection)
 
-    if ( playerSelection === "rock" && computerSelection === "scissors" ) {
-        playerScore++;
-    } else if ( playerSelection === "rock" && computerSelection === "paper" ) {
-        computerScore++;
-    } else if ( playerSelection === "scissors" && computerSelection === "rock" ) {
-        computerScore++;
-    } else if ( playerSelection === "scissors" && computerSelection === "paper" ) {
-        playerScore++;
-    } else if ( playerSelection === "paper" && computerSelection === "rock" ) {
-        playerScore++;
-    }else if ( playerSelection === "paper" && computerSelection === "scissors" ){
-        computerScore++;
-    };
+    // if ( playerSelection === "rock" && computerSelection === "scissors" ) {
+    //     playerScore++;
+    // } else if ( playerSelection === "rock" && computerSelection === "paper" ) {
+    //     computerScore++;
+    // } else if ( playerSelection === "scissors" && computerSelection === "rock" ) {
+    //     computerScore++;
+    // } else if ( playerSelection === "scissors" && computerSelection === "paper" ) {
+    //     playerScore++;
+    // } else if ( playerSelection === "paper" && computerSelection === "rock" ) {
+    //     playerScore++;
+    // }else if ( playerSelection === "paper" && computerSelection === "scissors" ){
+    //     computerScore++;
+    // };
 
-    let  scoreBoard = playerScore + " " + ":" + " " + computerScore;
+    // let  scoreBoard = playerScore + " " + ":" + " " + computerScore;
 
-    return alert(scoreBoard); 
+    // return alert(scoreBoard); 
 }
-
-
-// playRound ();
-// playRound ();
-// playRound ();
-// playRound ();
-// playRound ();
-
+    const btns = document.querySelectorAll('button')
+    btns.forEach(btn => btn.addEventListener('click', playRound));
 
 function matchScore () {
 
@@ -73,4 +58,4 @@ function matchScore () {
     }
 };
 
-matchScore();
+// matchScore();
